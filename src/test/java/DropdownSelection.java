08a3e283dfbdede21916666f75791d04e63dd627
puvi <puviarasan88@gmail.com>
2025-03-05 22:08:17 -0500

    Dropdown selection login updated.

---
+++ src/test/java/DropdownSelection.java
@@ -0,0 +1,20 @@
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.SelectOption;
+
+public class DropdownSelection {
+    public static void main(String[] args) {
+        Browser browser = Playwright.create()
+                                     .chromium()
+                                      .launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Page page = browser.newPage();
+        page.navigate("https://letcode.in/dropdowns");
+
+        page.locator("#fruits").selectOption("Apple");
+
+        page.locator("#superheros").selectOption(new String[]{"Aquaman", "Batman"});
+
+
+
+    }
+}
